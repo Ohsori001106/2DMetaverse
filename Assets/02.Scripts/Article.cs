79c2eefdb6de255bca3ce8e51fab55c36ab3295a
Ohsori001106 <surnameMin@Gmail.com>
2024-05-07 18:05:46 +0900

    제이슨으로 텍스트파일로 저장

--- Assets/02.Scripts/Article.cs
+++ Assets/02.Scripts/Article.cs
@@ -3,11 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// MVC 아키텍처 패턴
+// - Model (데이터와 그 데이터를 다루는 로직) -> Article
+// - View  (UI, 사용자 인터페이스)            -> UI_Article, UI_ArticleList
+// - Controller (관리자, Model <-> View 중재자) -> ArticleManager , 관리: 삽입,삭제,수정
+// 위 요소들(데이터, 시각적, 관리)의 간섭없이 독립적으로 개발할 수 있다.
+
 public enum ArticleType
 {
     Normal,
     Notice,
 }
+
+[Serializable]
 public class Article
 {
     public ArticleType ArticleType;
@@ -16,4 +24,15 @@ public class Article
     public string Content; // 글 내용
     public int Like; // 좋아요 개수
     public DateTime WriteTime; // 글 쓴 날짜 / 시간
+}
+
+[Serializable]
+public class ArticleData
+{
+    public List<Article> Data;
+
+    public ArticleData(List<Article> data)
+    {
+        Data = data;
+    }
 }
